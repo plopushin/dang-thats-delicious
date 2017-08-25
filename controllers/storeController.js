01390b31832e26695c0d86f43c47a866165f5a2b
plopushin <lopushin@ualberta.ca>
2017-08-25 11:33:39 -0700

    Tag page now shows stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -87,3 +87,15 @@ exports.getStoreBySlug = async (req, res, next) => {
   if(!store) return next();
   res.render('store', { store, title: store.name});
 };
+
+exports.getStoresByTag = async (req, res) => {
+  const tag = req.params.tag;
+  const tagQuery = tag || { $exists: true};
+
+  const tagsPromise = Store.getTagsList();
+  const storesPromise = Store.find({ tags: tagQuery});
+  //waiting for multiple promises
+
+  const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
+  res.render('tag', { tags: tags, title: 'Tags', tag, stores});
+};
