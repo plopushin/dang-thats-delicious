9bb33c4515830a65f072e6ac9e329e1e9c361ade
plopushin <lopushin@ualberta.ca>
2017-08-31 14:42:13 -0700

    Still trying to figure out this invalid email issue...

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,45 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
-// Do work here for all the routes
-
+const userController = require('../controllers/userController');
+//const authController = require('../controllers/authController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
-
 router.get('/add', storeController.addStore);
 
 router.post('/add',
   storeController.upload,
   catchErrors(storeController.resize),
   catchErrors(storeController.createStore)
- );
+);
 
-router.post('/add/:id', 
+router.post('/add/:id',
   storeController.upload,
   catchErrors(storeController.resize),
   catchErrors(storeController.updateStore)
-  );
+);
 
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
-
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
+router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
+
+// 1. Validate the registration data
+// 2. register the user
+// 3. we need to log them in
+router.post('/register',
+  userController.validateRegister,
+  userController.register
+  // we need to know about errors if 
+  // validation will be passed, but registration 
+  // will be failed in some reasons, e.g. second 
+  // registration with same email
+);
 
 module.exports = router;
