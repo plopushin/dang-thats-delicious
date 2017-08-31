148e8a09457d56131032c5ea9e894bad279014fd
plopushin <lopushin@ualberta.ca>
2017-08-31 15:00:18 -0700

    Fixed invalid email address issue, added register, login, and logout functionality

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
+
 //const authController = require('../controllers/authController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
@@ -28,6 +30,7 @@ router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 router.get('/login', userController.loginForm);
+router.post('/login', authController.login);
 router.get('/register', userController.registerForm);
 
 // 1. Validate the registration data
@@ -35,11 +38,14 @@ router.get('/register', userController.registerForm);
 // 3. we need to log them in
 router.post('/register',
   userController.validateRegister,
-  userController.register
+  userController.register,
+  authController.login
   // we need to know about errors if 
   // validation will be passed, but registration 
   // will be failed in some reasons, e.g. second 
   // registration with same email
 );
+
+router.get('/logout', authController.logout);
 
 module.exports = router;
