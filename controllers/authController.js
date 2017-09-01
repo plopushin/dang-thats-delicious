c902a3375081c0514e9901bf8f0a8b8d0ca4b3e0
plopushin <lopushin@ualberta.ca>
2017-09-01 16:59:59 -0700

    Update authcontroller and user model

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,3 +12,14 @@ exports.logout = (req, res) => {
   req.flash('success', 'You are now logged out!');
   res.redirect('/');
 };
+
+exports.isLoggedIn = (req, res, next) => {
+  //first check if user is authenticated
+  //passport method
+  if(req.isAuthenticated()) {
+    next();// they are logged in
+    return;
+  }
+  req.flash('error', 'You must be logged in to add a store!');
+  res.redirect('/login');
+}
